0b379f4f036c695d0d435e7dbe01543ca7871ecd
loveheenavasu <trivediheenalove@gmail.com>
2024-01-09 11:58:26 +0530

    fixed image loading and scroll issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,19 @@
 import Image from "next/image";
 import QuoteCard from "@/components/QuoteCard";
+import leftSideImg from "../../public/leftSide.png"
 
 export default function Home() {
+
   return (
     <main className="flex flex-col md:flex-row min-h-screen items-center justify-evenly p-4 md:p-24 text-white">
       <div className="flex-shrink-0 mb-4 md:mb-0 md:mr-8">
-        <Image
-          src="/leftSide.png"
-          alt="Quote Generator"
-          className="transparent"
-          width={340}
-          height={100}
-          // loading="lazy"
-          priority
-        />
+          <Image
+            src={leftSideImg}
+            alt="Quote Generator"
+            width={340}
+            priority
+            quality={100}
+          />
       </div>
       <QuoteCard />
     </main>
